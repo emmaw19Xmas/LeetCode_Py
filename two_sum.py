7240deea0ebd55694f26eae96f1baa417f7f33cb
18635 <emmaw4725@outlook.com>
2021-11-14 10:16:50 -0500

    1.Two Sum 11/14/2021

--- two_sum.py
+++ two_sum.py
@@ -1,10 +1,14 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        for i in range(0,len(nums)-1):
-            for j in range(i+1, len(nums)):
-                pass
+"""Emma - Fisrt Try"""
+from typing import List
+
+
+def twoSum(nums, target):
+    for i in range(0,len(nums)-1):
+        for j in range(i+1, len(nums)):
+            if nums[i]+nums[j] == target:
+                print(i,j)
+
+# nums = [3, 2, 4]
+# target = 7
+twoSum(nums=[3,2,4],target=7)
+
